6c63671011d788fada402ba1790adc43a210e819
Andrew Hammond <andrew@tremorlab.com>
2017-02-24 09:11:35 -0500

    Stub out LeoPhi code for pH readings
    * See: https://github.com/SparkysWidgets/LeoPhiBFW

--- doser.ino
+++ doser.ino
@@ -1,5 +1,7 @@
 #include <Button.h>        //https://github.com/JChristensen/Button
+#include <avr/eeprom.h> 
 
+#define PH_PIN 8
 #define RELAY_PIN 3        //Relay for activating dosing
 #define BUTTON_PIN 2       //Connect a tactile button switch (or something similar)
                            //from Arduino pin 2 to ground.
@@ -8,10 +10,14 @@
                            //switch is closed, this is negative logic, i.e. a high state
                            //means the button is NOT pressed. (Assuming a normally open switch.)
 #define DEBOUNCE_MS 10     //A debounce time of 20 milliseconds usually works well for tactile button switches.
-
 #define LED_PIN 13         //The standard Arduino "Pin 13" LED.
 #define LONG_PRESS 500     //We define a "long press" to be 500 milliseconds.
 #define BLINK_INTERVAL 50  //In the BLINK state, switch the LED every 50 milliseconds.
+
+//EEPROM trigger check
+#define WRITE_CHECK      0x1234 
+#define LEOPHI_VERSION   0x0002
+
 
 Button myBtn(BUTTON_PIN, PULLUP, INVERT, DEBOUNCE_MS);    //Declare the button
 
@@ -29,7 +35,42 @@ boolean readOnlyState;           //The readonly state (false = dont auto dose)
 boolean ledState;                //The current LED status
 unsigned long startMs;
 unsigned long ms;                //The current time from millis()
-unsigned long msLast;            //The last time the LED was switched
+unsigned long msLast;            //The last time the LED was switched     
+
+unsigned long adcMillis = 0;       
+int statusInterval = 1000;       // interval at which to blink or send updates(milliseconds)
+int adcReadInterval = 20;        // Our ADC read routine should be cycling at about ~50hz (20ms)
+
+const int adcMaxStep = 1023; //added this to allow for variable bit ranges (Remember Newer AVR adc architecture(e.g Atmega vs At90s) is meant for a divisor of 2^n-1!!!)
+const float vRef = 5.00; //Set our voltage reference (what is out Voltage at the Vin (+) of the ADC in this case an atmega32u4)
+const float opampGain = 5.25; //what is our Op-Amps gain (stage 1)
+
+//Rolling average this should act as a digital filter (IE smoothing)
+const int numPasses = 200; //Larger number = slower response but greater smoothing effect
+
+int passes[numPasses];    //Our storage array
+int passIndex = 0;          //what pass are we on? this one
+long total = 0;              //running total
+int pHSmooth = 0;           //Our smoothed pHRaw number
+
+//pH calc globals
+float milliVolts, pH; //using floats will transmit as 4 bytes over I2C
+
+//Continuous reading flag
+bool continousFlag,statusGFlag;
+
+
+//Our parameter, for ease of use and eeprom access lets use a struct
+struct parameters_T
+{
+  unsigned int WriteCheck;
+  int pH7Cal, pH4Cal, pH10Cal;
+  bool continous, statusLEDG;
+  float pHStep;
+} 
+params;
+
+
 
 void setup(void)
 {
@@ -37,7 +78,18 @@ void setup(void)
     
     pinMode(RELAY_PIN, OUTPUT);  //Set the relay pin as an output
     pinMode(LED_PIN, OUTPUT);    //Set the LED pin as an output
+    pinMode(PH_PIN, INPUT);      //Set the PH probe pin as an input
 
+    eeprom_read_block(&params, (void *)0, sizeof(params));
+    continousFlag = params.continous;
+    statusGFlag = params.statusLEDG;
+    if (params.WriteCheck != WRITE_CHECK) {
+      reset_Params();
+    }
+    // initialize smoothing variables to 0: 
+    for (int thisPass = 0; thisPass < numPasses; thisPass++)
+      passes[thisPass] = 0;
+    
     startMs = 0;
     STATE = HELLO_VERSION;
 }
@@ -47,6 +99,26 @@ void loop(void)
     ms = millis();               //record the current time
     myBtn.read();                //Read the button
 
+    if (ms - adcMillis > adcReadInterval) {
+      // save the last time you blinked the LED 
+      adcMillis = ms;
+  
+      total = total - passes[passIndex];
+      //grab our pHRaw this should pretty much always be updated due to our Oversample ISR
+      //and place it in our passes array this mimics an analogRead on a pin
+      digitalWrite(4, HIGH); 
+      passes[passIndex] = smoothADCRead(PH_PIN);
+      digitalWrite(4, LOW); //these will show our sampling fQ checking with scope!
+      total = total + passes[passIndex];
+      passIndex = passIndex + 1;
+      //Now handle end of array and make our rolling average portion
+      if (passIndex >= numPasses) {
+        passIndex = 0;
+      }
+      
+      pHSmooth = total/numPasses;
+    }  
+  
     switch (STATE) {
       case HELLO_VERSION:
         turnOnLED();
@@ -235,5 +307,38 @@ void fastBlink()
 {
   if (ms - msLast >= BLINK_INTERVAL)
     switchLED();
+}
+
+void calcpH()
+{
+ float temp = ((((vRef*(float)params.pH7Cal)/adcMaxStep)*1000)- calcMilliVolts(pHSmooth))/opampGain;
+ pH = 7-(temp/params.pHStep);
+}
+
+float calcMilliVolts(int numToCalc)
+{
+ float calcMilliVolts = (((float)numToCalc/adcMaxStep)*vRef)*1000; //pH smooth is our rolling average mine as well use it
+ return calcMilliVolts;
+}
+
+int smoothADCRead(int whichPin)
+{
+  //lets just take a reading and drop it, this should eliminate any ADC multiplexer issues
+  int throwAway = analogRead(whichPin);
+  int smoothADCRead = analogRead(whichPin);
+  return smoothADCRead;
+}
+
+void reset_Params(void)
+{
+  //Restore to default set of parameters!
+  params.WriteCheck = WRITE_CHECK;
+  params.statusLEDG = true;
+  params.continous = false; //toggle continuous readings
+  params.pH7Cal = 512; //assume ideal probe and amp conditions 1/2 of 1024
+  params.pH4Cal = 382; //using ideal probe slope we end up this many 10bit units away on the 4 scale
+  params.pH10Cal = 890;//using ideal probe slope we end up this many 10bit units away on the 10 scale
+  params.pHStep = 59.16;//ideal probe slope
+  eeprom_write_block(&params, (void *)0, sizeof(params)); //write these settings back to eeprom
 }
 
